5a072fa88b030432f7f66bc208132ee184120286
Alex Broad <46813168+alex-broad@users.noreply.github.com>
2024-09-28 19:09:01 +0100

    now with files actually added

---
+++ internal/reposted/hash.go
@@ -0,0 +1,39 @@
+package reposted
+
+import (
+	"fmt"
+	"image"
+	"net/http"
+
+	"github.com/corona10/goimagehash"
+)
+
+func hashImageFromURL(url string) (*goimagehash.ImageHash, error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != 200 {
+		return nil, fmt.Errorf("got %d downloading %s", res.StatusCode, url)
+	}
+	defer res.Body.Close()
+	m, _, err := image.Decode(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return goimagehash.AverageHash(m)
+}
+
+func findRepost(hashMap ImgHashPost, hash *goimagehash.ImageHash, distance int) (*goimagehash.ImageHash, error) {
+	for h := range hashMap {
+		loopHash := goimagehash.NewImageHash(h, goimagehash.AHash)
+		d, err := hash.Distance(loopHash)
+		if err != nil {
+			return nil, err
+		}
+		if d <= distance {
+			return loopHash, nil
+		}
+	}
+	return nil, nil
+}
---
+++ internal/reposted/history.go
@@ -0,0 +1,19 @@
+package reposted
+
+// func ProcessHistory(s *discordgo.Session) error {
+// 	for _, lpGuild  := range LastPosts {
+// 		for channelID, messageID := range lpGuild {
+// 			messages, err := s.ChannelMessages(channelID, 100,"", messageID,"")
+// 			if err != nil {
+// 				return err
+// 			}
+// 			for _, m := range messages {
+// 				ProcessMessage(m)
+// 				AddToDB()
+// 				SendMessage()
+// 			}
+
+// 		}
+// 	}
+// 	return nil
+// }
