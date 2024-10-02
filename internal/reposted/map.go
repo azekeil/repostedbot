9bb134930d874b3215d704c4a81a2cd2a53a6607
Alex Broad <46813168+alex-broad@users.noreply.github.com>
2024-10-02 23:30:12 +0100

    fixed using RWMutex.. missing RUnlock() :(

--- internal/reposted/map.go
+++ internal/reposted/map.go
@@ -2,54 +2,71 @@ package reposted
 
 import (
 	"iter"
+	"sync"
 )
 
 type SafeMap[K comparable, V any] struct {
 	M map[K]V
-	l *SafeRWMutex
+	l *sync.RWMutex
 }
 
 func NewSafeMap[K comparable, V any]() *SafeMap[K, V] {
 	return &SafeMap[K, V]{
 		M: map[K]V{},
-		l: &SafeRWMutex{},
+		l: &sync.RWMutex{},
 	}
 }
 
 func (s *SafeMap[K, V]) Get(key K) V {
-	s.l.SRLock()
+	if s.l == nil {
+		s.l = &sync.RWMutex{}
+	}
+	s.l.RLock()
 	defer s.l.RUnlock()
 	return s.M[key]
 }
 
 func (s *SafeMap[K, V]) Get2(key K) (V, bool) {
-	s.l.SRLock()
+	if s.l == nil {
+		s.l = &sync.RWMutex{}
+	}
+	s.l.RLock()
 	defer s.l.RUnlock()
 	v, ok := s.M[key]
 	return v, ok
 }
 
 func (s *SafeMap[K, V]) Set(key K, value V) {
-	s.l.SLock()
+	if s.l == nil {
+		s.l = &sync.RWMutex{}
+	}
+	s.l.Lock()
 	s.M[key] = value
 	s.l.Unlock()
 }
 
 func (s *SafeMap[K, V]) Len() int {
-	s.l.SRLock()
+	if s.l == nil {
+		s.l = &sync.RWMutex{}
+	}
+	s.l.RLock()
 	defer s.l.RUnlock()
 	return len(s.M)
 }
 
 func (s *SafeMap[K, V]) Iter() iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
-		// s.l.RLock()
+		if s.l == nil {
+			s.l = &sync.RWMutex{}
+		}
+		s.l.RLock()
 		for k, v := range s.M {
-			// s.l.RUnlock()
+			s.l.RUnlock()
 			if !yield(k, v) {
 				return
 			}
-			// s.l.RLock()
+			s.l.RLock()
 		}
+		s.l.RUnlock()
 	}
 }
