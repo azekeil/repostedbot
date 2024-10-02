d094cd3961fb45000f6c6f1d7c4d954f0707adb8
Alex Broad <46813168+alex-broad@users.noreply.github.com>
2024-10-02 02:28:02 +0100

    working with no read locks

--- internal/reposted/map.go
+++ internal/reposted/map.go
@@ -6,52 +6,51 @@ import (
 )
 
 type SafeMap[K comparable, V any] struct {
-	m map[K]V
+	M map[K]V
 	l *sync.RWMutex
 }
 
 func NewSafeMap[K comparable, V any]() *SafeMap[K, V] {
 	return &SafeMap[K, V]{
-		m: map[K]V{},
+		M: map[K]V{},
 		l: &sync.RWMutex{},
 	}
 }
 
 func (s *SafeMap[K, V]) Get(key K) V {
-	s.l.RLock()
-	defer s.l.RUnlock()
-	return s.m[key]
+	// s.l.RLock()
+	// defer s.l.RUnlock()
+	return s.M[key]
 }
 
 func (s *SafeMap[K, V]) Get2(key K) (V, bool) {
-	s.l.RLock()
-	defer s.l.RUnlock()
-	v, ok := s.m[key]
+	// s.l.RLock()
+	// defer s.l.RUnlock()
+	v, ok := s.M[key]
 	return v, ok
 }
 
 func (s *SafeMap[K, V]) Set(key K, value V) {
 	s.l.Lock()
-	s.m[key] = value
+	s.M[key] = value
 	s.l.Unlock()
 }
 
 func (s *SafeMap[K, V]) Len() int {
-	s.l.RLock()
-	defer s.l.RUnlock()
-	return len(s.m)
+	// s.l.RLock()
+	// defer s.l.RUnlock()
+	return len(s.M)
 }
-
 
 func (s *SafeMap[K, V]) Iter() iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
-		s.l.RLock()
-		for k, v := range s.m {
-			s.l.RUnlock()
+		// s.l.RLock()
+		for k, v := range s.M {
+			// s.l.RUnlock()
 			if !yield(k, v) {
 				return
 			}
-			s.l.RLock()
+			// s.l.RLock()
 		}
 	}
 }
